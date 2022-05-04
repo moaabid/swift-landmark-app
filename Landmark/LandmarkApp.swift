928225913e91cb43ab7ffff39b68827ec6ceb67f
Mohammed Aabid A <mohammed@mavencart.com>
2022-05-04 09:21:41 +0530

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by Mohammed aabid on 04/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
